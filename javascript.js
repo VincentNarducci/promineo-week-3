ad85d57ecbce54b4077afc3627ab3ae6cc620c56
Vincent Narducci <89147658+VincentNarducci@users.noreply.github.com>
2021-09-08 00:45:56 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,24 +1,24 @@
 /* 1.   Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93. */
-var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 console.log(ages);
 
 /* a.   Programmatically subtract the value of the first element in the array from the value in the last element of the array.
 Print the result to the console. */
-var agesLength = ages.length;
-var lastMinusFirst = ages[(agesLength-1)]-ages[0];
+let agesLength = ages.length;
+let lastMinusFirst = ages[(agesLength-1)]-ages[0];
 console.log("The last ages array element minus the first is: " + lastMinusFirst);
 
 /* b.	Add a new age to your array and repeat the step above to ensure it is dynamic  */
 ages.push(42);
 console.log(ages);
-var agesLength = ages.length;
-var lastMinusFirst = ages[(agesLength-1)]-ages[0];
+agesLength = ages.length;
+lastMinusFirst = ages[(agesLength-1)]-ages[0];
 console.log("The last ages array element minus the first is: " + lastMinusFirst);
 
 /* c.	Use a loop to iterate through the array and calculate the average age. Print the result to the console. */
-var total = 0;
-var avg = 0;
-for (var i = 0; i < ages.length; i++){
+let total = 0;
+let avg = 0;
+for (let i = 0; i < ages.length; i++){
     avg = (total += ages[i]) / ages.length;
 } 
 console.log(
@@ -32,17 +32,17 @@ console.log(names);
 /* a.	Use a loop to iterate through the array and calculate the average number of letters per name.
 Print the result to the console */
 
-var total = 0;
-var avg = 0;
-for (var i = 0; i < names.length; i++){
-   avg = (total += names[i].length) / names.length;
+for (let i = 0; i < names.length; i++){
+    let total = 0;
+    let avg = 0;
+    avg = (total += names[i].length) / names.length;
 }
 console.log("The average # of letters per name is: " + avg.toFixed(1));
 
 /* b.	Use a loop to iterate through the array again and concatenate all the names together, separated by spaces,
 and print the result to the console. */
-var firstName = "";
-for (var i = 0; i < names.length-1; i++){
+let firstName = "";
+for (let i = 0; i < names.length-1; i++){
     firstName += names[i] + " ";
 }
 /* leave out last name from the loop and add it to the end so we don't have a space after the last name */
@@ -60,8 +60,8 @@ console.log("Example: " +names[0]);
 /* 5.	Create a new array called nameLengths. Write a loop to iterate over the previously created names array
 and add the length of each name to the nameLengths array. */
 let nameLengths = [];
-var letterCount = 0;
-for (var i = 0; i < names.length; i++){
+let letterCount = 0;
+for (let i = 0; i < names.length; i++){
     letterCount = names[i].length;
     nameLengths.push(letterCount);
  }
@@ -75,8 +75,8 @@ console.log(nameLengths); */
 
 /* 6.	Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array.
 Print the result to the console. */
-var letterSum = 0;
-for (var i = 0; i < nameLengths.length; i++){
+let letterSum = 0;
+for (let i = 0; i < nameLengths.length; i++){
     letterSum += nameLengths[i];
 }
 console.log("The sum of the elements of the nameLengths array is: " + letterSum);
@@ -84,8 +84,8 @@ console.log("The sum of the elements of the nameLengths array is: " + letterSum)
 /* 7.	Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times.
 (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’). */
 function stringRepeat(word, n){
-    var s = "";
-    for (var i = 0; i < n; i++){
+    let s = "";
+    for (let i = 0; i < n; i++){
        s += word;
     }
     return console.log(s);
@@ -103,8 +103,8 @@ fullName("Rick", "James");
 /* 9.	Write a function that takes an array of numbers and returns true if the sum of all the numbers
 in the array is greater than 100. */
 function moreThan100(arr){
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
         sum += arr[i];
     }
     if (sum > 100){
@@ -116,9 +116,9 @@ moreThan100([5, 10, 15, 20, 25, 30]);
 
 /* 10.	Write a function that takes an array of numbers and returns the average of all the elements in the array. */
 function avgArrayElements(arr){
-    var total = 0;
-    var avg = 0;
-    for (var i = 0; i < arr.length; i++){
+    let total = 0;
+    let avg = 0;
+    for (let i = 0; i < arr.length; i++){
         avg = (total += arr[i]) / arr.length;
     }
     return console.log("The average of the array elements is: " + avg);
@@ -129,14 +129,14 @@ avgArrayElements([20, 40, 60, 80, 100]);
 /* 11.	Write a function that takes two arrays of numbers and returns true if the average of the elements
 in the first array is greater than the average of the elements in the second array. */
 function isArrayAvgGreater(arr1, arr2){
-    var tot1 = 0;
-    var avg1 = 0;
-    var tot2 = 0;
-    var avg2 = 0;
-    for (var i = 0; i < arr1.length; i++){
+    let tot1 = 0;
+    let avg1 = 0;
+    let tot2 = 0;
+    let avg2 = 0;
+    for (let i = 0; i < arr1.length; i++){
         avg1 = (tot1 += arr1[i]) / arr1.length;
     }
-    for (var i = 0; i < arr2.length; i++){
+    for (let i = 0; i < arr2.length; i++){
         avg2 = (tot2 += arr2[i]) / arr2.length;
     }
     if (avg1 > avg2){
@@ -162,7 +162,7 @@ willByDrink(true, 50);
 /* this function flips a coin by generating a random number between 0 and 1 using the js Math.random function (inclusive of 0 but not 1)
 the outcome of the flip is determined by whether or not the random number is greater than .5 */
 function coinFlip(){
-    var randomNum = Math.random();
+    let randomNum = Math.random();
     if (randomNum >= 0 && randomNum < .5){
     return console.log("Heads");
     } return console.log("Tails");
